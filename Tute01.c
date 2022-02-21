5cb4803b8c7712a3f20176c2929413a2a54c848a
M.A.Vimankith <100081824+it21322662@users.noreply.github.com>
2022-02-21 05:32:42 +0000

    completed exercise 1

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,19 @@
 #include <stdio.h>
 
 int main() {
-  
+  float sub1,sub2;
+  float avg;
+
+  printf("enter marks of sub1 :");
+  scanf("%f",&sub1);
+
+  printf("enter marks of sub2 :");
+  scanf("%f",&sub2);
+
+     avg=(sub1+sub2)/2;
+
+  printf("average is : %.2f",avg);
+
   return 0;
 }
 
